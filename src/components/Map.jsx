caa18e6bf93d82bdcb9c00257121ac261bde587a
sachin-warude <sachinwarude7620@gmail.com>
2026-02-13 15:41:00 +0530

    Implemented Map Functionality with leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Map.module.css';
+import 'leaflet/dist/leaflet.css';
+
 import { useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useCity } from '../context/CityProvider';
+
 export default function Map() {
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const navigate = useNavigate();
+  const { cities } = useCity();
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      Map
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map(city => (
+          <Marker position={[city.position.lat, city.position.lng]}>
+            <Popup>
+              {city.emoji} <br /> {city.cityName}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
